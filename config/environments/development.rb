e48fb0735c2e01187b93a1c0f6689422dbc6d197
adriannicolai <anlumbad@gmail.com>
2022-09-25 21:14:01 +0800

    hide the logger for views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Hide the rendering of asests
+  config.action_view.logger = nil
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
