24d16c5ced3e96f63fc698cd7c0756e55d0e96bd
adriannicolai <anlumbad@gmail.com>
2022-09-26 10:21:52 +0800

    updated helper for check_fields

--- server/app/helpers/application_helper.rb
+++ server/app/helpers/application_helper.rb
@@ -4,14 +4,14 @@ module ApplicationHelper
     # Requires: required_fields - [], optional_fields - []
     # Last updated at: September 26, 2022
     # Owner: Adrian
-    def check_fields(required_fields, optional_fields = [], params)
-        response_data = { :status => false, :result => {}, :error > nil }
+    def check_fields(required_fields = [], optional_fields = [], params)
+        response_data = { :status => false, :result => {}, :error => nil }
 
         begin
             invalid_fields = []
             all_fields     = required_fields + optional_fields
 
-            params.each do |key|
+            all_fields.each do |key|
                 if params[key].present?
                     response_data[:result][key] = params[key].is_a?(String) ? params[key].strip : params[key]
                 elsif required_fields.include?(key)
@@ -20,7 +20,7 @@ module ApplicationHelper
                 end
             end
 
-            response_data.merge!(invalid_fields.empty? ? { :status => true, :result => response_data[:result].symbolize_keys } ? { :result => invalid_fields })
+            response_data.merge!(invalid_fields.empty? ? { :status => true, :result => response_data[:result].symbolize_keys } : { :result => invalid_fields, :error => "Missing required fields" })
         rescue Exception => ex
             response_data[:error] = ex.message
         end
