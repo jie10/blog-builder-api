7e08d490c01782941868130cd9cf8144d88b6d89
adriannicolai <anlumbad@gmail.com>
2022-09-26 08:19:23 +0800

    added user helper and user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,14 @@
 class UserController < ApplicationController
 	def login
-		
+		response_data = { :status => false, :result => {}, :error => nil }
+
+		begin
+
+		rescue Exception => ex
+			response_data[:error] = ex.message
+		end
+
+		render :json => response_data
 	end
 
 	def register
