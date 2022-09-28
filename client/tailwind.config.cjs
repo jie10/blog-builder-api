2978c69e9db874365a8beb939aafaf365efd7e27
jie10 <benjieburillo10@gmail.com>
2022-09-28 09:40:09 +0800

    add plugins

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [   "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",],
+    "./src/**/*.{vue,js,ts,jsx,tsx}",],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms')
+  ],
 }
