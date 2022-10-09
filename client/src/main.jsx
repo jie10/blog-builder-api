31a748c544736e3f61d5f5adcae76b490bdaf8ed
Benjie Jabir Burillo <benjieburillo10@gmail.com>
2022-10-09 23:34:30 +0800

    setup routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,14 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "tailwindcss/tailwind.css"
-import "./index.css"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import ErrorPage from "./components/errors/ErrorPage.jsx"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
+    errorElement: <ErrorPage/>,
   },
 ])
 
