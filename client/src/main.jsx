ba26637d2b136e06fd914b49725bbd444bc82915
jie10 <benjieburillo10@gmail.com>
2022-10-12 08:18:28 +0800

    update routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,25 +1,43 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import "tailwindcss/tailwind.css"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import ErrorPage from "./components/errors/ErrorPage.jsx"
-import Backend from "./components/defaults/backend.jsx"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import 'tailwindcss/tailwind.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ErrorPage from './components/errors/ErrorPage.jsx'
+import Backend from './components/defaults/backend.jsx'
+import Client from './components/defaults/client.jsx'
+import Blogs from './views/dashboard/blogs.jsx'
+import Profile from './views/dashboard/profile.jsx'
+import Home from './views/dashboard/home.jsx'
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App/>,
+    path: '/',
+    element: <Client/>,
     errorElement: <ErrorPage/>,
   },
   {
-    path: "dashboard",
+    path: 'dashboard',
     element: <Backend/>,
+    children: [
+      {
+        path: 'dashboard',
+        element: <Home/>,
+      },
+      {
+        path: 'blogs',
+        element: <Blogs/>,
+      },
+      {
+        path: 'profiles',
+        element: <Profile/>,
+      },
+    ],
   },
 ])
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={ router }/>
   </React.StrictMode>,
 )
